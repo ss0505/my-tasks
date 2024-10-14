d6f51f037ba5d6f3c9a48f04d20c72db24620a10
unknown <soya.sudoh@gmail.com>
2024-10-14 17:16:48 +0900

    add recoil settings

---
+++ src/lib/RecoilContextProvider.js
@@ -0,0 +1,6 @@
+'use client'
+import { RecoilRoot } from 'recoil'
+const RecoilContextProvider = ({ children }) => {
+  return <RecoilRoot>{children}</RecoilRoot>
+}
+export default RecoilContextProvider
